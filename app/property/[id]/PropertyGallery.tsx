99217a5788e7ae56c0f53c2c16db24a76da0637b
David Zak <dzak1@Lenovo.com>
2026-09-17 15:04:37 +0200

    Fix gallery images

--- app/property/[id]/PropertyGallery.tsx
+++ app/property/[id]/PropertyGallery.tsx
@@ -28,7 +28,11 @@ export default function PropertyGallery({
   return (
     <section>
       <div className="relative">
-        {selectedImage}
+        <img
+          src={selectedImage}
+          alt={title}
+          className="w-full h-[500px] object-cover"
+        />
 
         <span className="absolute bottom-4 right-4 bg-black/70 text-white px-4 py-2 rounded-xl text-sm">
           {images.indexOf(selectedImage) + 1} / {images.length}
@@ -57,7 +61,11 @@ export default function PropertyGallery({
                   index + 1
                 }`}
               >
-                {imageUrl}
+                <img
+                  src={imageUrl}
+                  alt={`${title} ${index + 1}`}
+                  className="w-full h-24 object-cover"
+                />
               </button>
             );
           })}
