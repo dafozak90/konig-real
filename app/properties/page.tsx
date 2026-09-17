bb128bd7c564c7d59a12ef85cba1d2b08df8d0d3
David Zak <dzak1@Lenovo.com>
2026-09-17 14:27:41 +0200

    debug properties

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -12,15 +12,6 @@ export default async function PropertiesPage() {
     if (error) {
         return <div>Chyba: {error.message}</div>;
     }
-    return (
-2
-<pre>
-3
-{JSON.stringify(properties, null, 2)}
-4
-</pre>
-5
-);
 
     return (
         <main className="max-w-6xl mx-auto p-8">
